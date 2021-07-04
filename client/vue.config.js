7161fd8d96c514bd0a7c8430ac5d7d12930b9511
Fernando Barboza <falandocomofernando@gmail.com>
2021-07-04 03:31:57 -0300

    add hot reload and removed useless files

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     devServer: {
-        port: 8081
+        port: 8081,
+        watchOptions: {
+            ignored: '/node_modules/',
+            aggregateTimeout: 300,
+            poll: 1000,
+          },
     }
 }
