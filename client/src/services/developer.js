724fcab8ee2a0121a39877e963e4e2a5be8be3e7
Fernando Barboza <falandocomofernando@gmail.com>
2021-07-05 18:21:40 -0300

    client: add remove api call

--- client/src/services/developer.js
+++ client/src/services/developer.js
@@ -8,9 +8,12 @@ const create = developer => axios.post("/developers", developer);
 
 const update = (developer, developerId) => axios.put(`/developers/${developerId}`, developer);
 
+const remove = developerId => axios.delete(`/developers/${developerId}`);
+
 export default {
     getAll,
     getById,
     create,
-    update
+    update,
+    remove
 }
